5cab1f7b171d69280b7262fcafcc5bc980714a48
Flavio Silva <fal_silva@hotmail.com>
2023-05-23 21:52:50 -0300

    Condição simples IF

---
+++ aula08/ex008.js
@@ -0,0 +1,9 @@
+//console.log("O console funcionou corretamente!")
+
+var vel = 65
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+if (vel > 60) { //Condição Simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
