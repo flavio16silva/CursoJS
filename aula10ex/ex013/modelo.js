0a3ff06599e587e509bb959a8ce906ea1eb423a3
Flavio Silva <fal_silva@hotmail.com>
2023-05-26 23:49:59 -0300

    Comentários

--- aula10ex/ex013/modelo.js
+++ aula10ex/ex013/modelo.js
@@ -4,10 +4,10 @@ function carregar () {
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date() //Pegando a data do dia
-//var hora = data.getHours() // Pegando a hora atual
+var hora = data.getHours() // Pegando a hora atual
 var min = data.getMinutes()
 var segundo = data.getSeconds()
-var hora = 22
+//var hora = 22
 msg.innerHTML = `Agora são ${hora} horas  ${min} minutos e ${segundo} segundos.`
 
 
@@ -15,17 +15,17 @@ if (hora >= 0 && hora < 12) {
   //Bom dia 
   img.src = 'manha.jpg'
   document.body.style.background = '#c0d8e2'
-  mensagem.innerHTML = "Bom dia!"
+  mensagem.innerHTML = "Bom dia!" //Mensagem do dia
 } else if (hora >= 12 && hora <= 18) {
   //Boa tarde
   img.src = 'tarde.jpg'
   document.body.style.background = '#be8666'
-  mensagem.innerHTML = "Boa tarde!"
+  mensagem.innerHTML = "Boa tarde!" //Mensagem da tarde
 } else {
   //Boa noite
   img.src = 'noite.jpg' //Trazendo a imagem 
   document.body.style.background = '#763762' //Mudando a cor de fundo
-  mensagem.innerHTML = "Boa noite!"
+  mensagem.innerHTML = "Boa noite!" //Mensagem da noite
 }
 
 
