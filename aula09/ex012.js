ed3949f4ba05d1324e4e220972e58b2f4437b9c7
Flavio Silva <fal_silva@hotmail.com>
2023-05-27 11:28:22 -0300

    comentarios

--- aula09/ex012.js
+++ aula09/ex012.js
@@ -91,25 +91,6 @@ switch(diaSem) {
 
 *O  default:  serve para, se NENHUM dos CASOS SERVIR, executa-se o  default:. Caso o  default:  esteja por ÚLTIMO, também não é necessário pôr o break. Entretanto, caso esteja, por exemplo, em 1°, é NECESSÁRIO pôr o break  pois, senão, ele irá executar os PRÓXIMOS CASOS até encontrar um break.
 *A estrutura  switch() é um pouco limitada pois, no VALOR de cada CASO, só é possível utilizar "NÚMEROS INTEIROS e STRINGS".
-____________________________
 
-O Construtor  new Date()  retorna um OBJETO de DATA com: DIA da SEMANA, DATA, HORA ATUAL e FUSO HORÁRIO. Ex:
-Fri Nov 04 2022 08:30:47 GMT-0300 (Horário Padrão de Brasília)
-
-  Por ser um Construtor,  new Date()  permite-nos criar OBJETOS de DATA ESPECÍFICOS A PARTIR DELE. Para fazer isto, primeiramente, para facilitar, 
-  guarda-se o new Date() dentro de uma variável, desta forma:
-
-  data = new Date()
-  E depois você pode usar algum destes MÉTODOS:
-
-data.getFullYear()         Retorna o ANO atual (yyyy)
-data.getMonth()	           Retorna o MÊS atual (0-11)
-data.getDate()	           Retorna o DIA atual (1-31)
-data.getDay()	             Retorna o DIA da SEMANA atual como um NÚMERO (0-6) *0 = domingo; 6 = sábado.
-data.getHours()	           Retorna a HORA atual (0-23)
-data.getMinutes()          Retorna os MINUTOS atuais (0-59)
-data.getSeconds()          Retorna os SEGUNDOS atuais (0-59)
-data.getMilliseconds()     Retorna os MILISSEGUNDOS atuais (0-999)
-data.getTime()	           Retorna os MILISSEGUNDOS desde o dia 1 de Janeiro de 1970 (WTF? xD)
 
 */
