e05276680686091ef96066e454cb8d9513cd0397
Flavio Silva <fal_silva@hotmail.com>
2023-05-26 23:48:48 -0300

    Inclusão de min e seg

--- aula10ex/ex013/modelo.js
+++ aula10ex/ex013/modelo.js
@@ -6,22 +6,28 @@ var img = window.document.getElementById('imagem')
 var data = new Date() //Pegando a data do dia
 //var hora = data.getHours() // Pegando a hora atual
 var min = data.getMinutes()
-var hora = 23
-msg.innerHTML = `Agora são ${hora} horas e ${min} minutos.`
+var segundo = data.getSeconds()
+var hora = 22
+msg.innerHTML = `Agora são ${hora} horas  ${min} minutos e ${segundo} segundos.`
 
 
 if (hora >= 0 && hora < 12) {
   //Bom dia 
   img.src = 'manha.jpg'
   document.body.style.background = '#c0d8e2'
+  mensagem.innerHTML = "Bom dia!"
 } else if (hora >= 12 && hora <= 18) {
   //Boa tarde
   img.src = 'tarde.jpg'
   document.body.style.background = '#be8666'
+  mensagem.innerHTML = "Boa tarde!"
 } else {
   //Boa noite
   img.src = 'noite.jpg' //Trazendo a imagem 
   document.body.style.background = '#763762' //Mudando a cor de fundo
+  mensagem.innerHTML = "Boa noite!"
 }
+
+
 
 }
