744ec244fe91fc7b66cd6d659b457a84e2f40531
Flavio Silva <fal_silva@hotmail.com>
2023-05-31 13:30:45 -0300

    While e DO

---
+++ aula11/ambiente.js
@@ -0,0 +1,56 @@
+//Segunda Estrutura
+
+var c = 1                    
+ do {                         //Faça
+  console.log(`Passo ${c}` )
+  c++                        // c = c + 1
+} while (c <= 6)             //Enquanto for menor ou igual a 6
+
+
+
+/* Primeira Estrutura
+var c = 1                    //varivel com contador começando em 1
+while (c <= 6){
+  console.log(`Passo ${c}` )
+  c++                       // c = c + 1
+}
+
+*/
+
+
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
+
+/*
+Estruturas de  (Laços || Iterações || Repetições || Looping)  :
+
+  while (enquanto)
+
+Esta estrutura faz o teste lógico ANTES de rodar o programa.
+
+Enquanto (while) condição1 for VERDADEIRA, execute o Bloco A.
+
+Ilustração:
+while (condição1) {
+Bloco A
+}
+____________________________
+
+  do/while (faça/enquanto)
+
+Esta estrutura faz o teste lógico DEPOIS de rodar o programa.
+
+Faça (do) a execução do Bloco A, enquanto (while) condição1 for VERDADEIRA.
+
+Ilustração:
+do {
+Bloco A
+} while (condição1)
+
+
+*/
