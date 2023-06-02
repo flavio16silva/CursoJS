ddfe0b9c666f6efdcdc4d863eb5f023a54823fb7
Flavio Silva <fal_silva@hotmail.com>
2023-06-02 14:30:40 -0300

    criando o contador JS

---
+++ aula12ex/ex015/modelo.js
@@ -0,0 +1,41 @@
+function contar() {
+  //Criando as variaveis que pegarão os valores de acordo "id"
+  let inicio = document.getElementById('txti')
+  let fim = document.getElementById('txtf')
+  let passo = document.getElementById('txtp')
+  let res = document.getElementById('res')
+
+    //Verificando se estão faltando os dados.  
+  if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    res.innerHTML = ('Não consigo contar!')
+    //window.alert('[ERRO] FALTAM INFORMAÇÕES!')
+  } else {
+    //alert('INFORMAÇÕES CORRETAS!')
+    res.innerHTML = 'Contando: ... <br>'
+
+      /* === Criando 3 variaveis númericas === */
+    let i = Number(inicio.value) //Convertendo os valores das variaveis acima
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+
+    /* === Para zero e negativos não serem considerados === */
+    if (p <= 0) {
+      window.alert('Valor do passo inválido! Considerando PASSO = 1')
+      p = 1
+    }
+
+    if (i < f) {
+      //Contagem Crescente
+      for(let c = i; c <= f ; c += p){
+        res.innerHTML += ` ${c} \u{27A1}` /* Unicode só funcina com Crases. */
+  }
+    } else {
+      //Contagem Decrescente
+        for(let c = i; c >= f; c -= p){
+          res.innerHTML += ` ${c} \u{27A1}`
+        }
+    }
+     res.innerHTML += `\u{2705}`
+  }
+
+}
