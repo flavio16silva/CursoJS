1b3dcac94bae363c420304d4e1bf3e8a292a5cec
Flavio Silva <fal_silva@hotmail.com>
2023-06-06 14:03:04 -0300

    Array, valores, posição no JS

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+let num = [5, 2, 9, 8, 3]
+// A ordem que for inserida o sort() e o push incluenciará no retorno dado pelo JS.
+//num.sort()                                               //colocar em ordem crescente
+//num.push(1)                                              // incluir o valor 1 no vetor
+//console.log(num)
+//console.log(`O vetor tem ${num.length} posições`)
+//console.log(`O primeiro valor do vetor é: ${num[0]}`)
+
+
+let pos = num.indexOf(7)
+  if (pos == -1) {
+    console.log('O valor desejado não foi encontrado')
+  } else {
+    console.log(`O valor está na posição: ${pos}`)
+  }
+  
+
