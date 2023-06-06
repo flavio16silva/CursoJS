28eadac65bc07bb6d08f14159900cf9a0e7c02dc
Flavio Silva <fal_silva@hotmail.com>
2023-06-05 22:16:54 -0300

    uso de while para for

--- aula12ex/ex016/modelo.js
+++ aula12ex/ex016/modelo.js
@@ -6,14 +6,15 @@ function calcular() {
     window.alert('Digite um número válido!') //Em caso de valor vazio
   }else {
     var n = Number(numero.value) //Convertendo o 'número' que será digitado em: 'txtnum' de texto para número
-    var c = 1
+    //var c = 1
     tab.innerHTML = ``           //Antes de começar a tabuada, a area será limpa.
 
-    while (c <= 10){ //Enquanto o contador for <= 10
+    for( c = 1; c <= 10; c++) {
+    //while (c <= 10){ //Enquanto o contador for <= 10
       let item = document.createElement('option') //adicionando elementos 'options' como no HTML
       item.text = `${n} x ${c} = ${n*c}` //Inserindo os valores dentro do options - Uso da Template String
       tab.appendChild(item)              // Aparecer o valor, ou seja, adicionar um elemento filho
-      c++                                //Incrementando o valor de 'c' para gerar a contagem
+      //c++                                //Incrementando o valor de 'c' para gerar a contagem
     }
   }
     
